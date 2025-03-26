4d8cfafaa91c1de0878b2f4552d2be6304ca067b
yash7800 <yasharmaa0202@gmail.com>
2025-03-26 15:46:44 +0530

    commit

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+const nextConfig = {
+  output: "export",
 };
+
+export default nextConfig;
+
 
 export default nextConfig;
