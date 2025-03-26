43d518f19ecab47fd6b32602f09a2ccde2f2608a
yash7800 <yasharmaa0202@gmail.com>
2025-03-26 15:51:59 +0530

    changes

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -2,11 +2,13 @@
 // const nextConfig: NextConfig = {
 //   /* config options here */
 // };
+
 const nextConfig = {
   output: "export",
+  basePath: "/Portfolio", // Replace 'portfolio' with your repo name
 };
 
-export default nextConfig;
+
 
 
 export default nextConfig;
